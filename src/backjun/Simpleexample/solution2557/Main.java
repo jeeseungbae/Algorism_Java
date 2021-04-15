1ff85280166f1b0e9ed2c8af619b992a1ac67970
jeeseungbae <https://github.com/jeeseungbae/TIMD_project.git>
2021-04-15 17:01:27 +0900

    type : example_solution
    푼 문제
    10869
    2750
    2557
    
    못푼 문제
    1236

---
+++ src/backjun/Simpleexample/solution2557/Main.java
@@ -0,0 +1,21 @@
+package backjun.Simpleexample.solution2557;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        Main main = new Main();
+        main.solution();
+    }
+
+    public void solution() throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+//        StringTokenizer st = new StringTokenizer(br.readLine());
+        String[] a = br.readLine().split(" ");
+
+        System.out.println(Integer.parseInt(a[0])+Integer.parseInt(a[1]));
+
+    }
+}
---
+++ src/backjun/Simpleexample/solution2750/Main.java
@@ -0,0 +1,22 @@
+package backjun.Simpleexample.solution2750;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        Main main = new Main();
+        main.solution();
+    }
+
+    public void solution() throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String a = br.readLine();
+
+        System.out.println(a);
+
+    }
+}
