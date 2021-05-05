97c153fe8cc77052c89f026c28194cc099dccf78
seungbae <sdgh1442@gmail.com>
2021-05-05 17:34:00 +0900

    단체사진찍기 - 미해결

---
+++ src/programmers/level2/단체사진찍기/Main.java
@@ -0,0 +1,10 @@
+package programmers.level2.단체사진찍기;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Solution s = new Solution();
+
+        System.out.println(s.solution(2,new String[]{"N~F=0", "R~T>2"}));
+    }
+}
